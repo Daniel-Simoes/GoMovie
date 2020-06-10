64c0467a5432132704950ada5cad3d5054c0ee16
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-06-10 08:38:10 +0100

    Feature finding a movie

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import requests
 import json
 
-try:
-    req = requests.get('http://www.omdbapi.com/?t=The+Matrix&apikey=79174ddd')
-except:
-    print("Sorry, We've had a connetions' problem")
-    exit()
 
-Movie_List = json.loads(req.text)
+def user_request(title):
+    try:
+        req = requests.get(
+            'http://www.omdbapi.com/?t=' + title + 'YOUR API KEY')
+        movie_list = json.loads(req.text)
+        return movie_list
+    except:
+        print("Sorry, We've had a connetions' problem")
+        return None
 
-print(Movie_List['Title'])
+
+def show_details(movie):
+    print('Title:', movie['Title'])
+    print('Year:', movie['Year'])
+    print('Director:', movie['Director'])
+    print('Actors:', movie['Actors'])
+    print('Rating:', movie['imdbRating'])
+    print('')
+
+
+sign_out = False
+while not sign_out:
+    title_input = input(
+        'Type a movie title to find the informations or OUT to exit the application: ')
+    if title_input == 'OUT':
+        sign_out = True
+        print('we hope to have helped your research')
+    else:
+        movie = user_request(title_input)
+        if movie['Response'] == 'False':
+            print('Sorry, we did not find this movie. Are you sure that title is rigth?')
+        else:
+            show_details(movie)
